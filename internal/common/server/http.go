dc28fe875a253f676fd6b9b94d3f9fca629dc181
Steven Eckhoff <steven.eckhoff.opensource@gmail.com>
2020-11-26 01:30:29 -0600

    Add server package

---
+++ internal/common/server/http.go
@@ -0,0 +1,84 @@
+// Package server implements a library helping with basic HTTP server operations.
+package server
+
+import (
+	"context"
+	firebase "firebase.google.com/go"
+	"github.com/go-chi/chi"
+	"github.com/sirupsen/logrus"
+	"github.com/steven-eckhoff/gopher-cache-open/internal/common/auth"
+	"github.com/steven-eckhoff/gopher-cache-open/internal/common/logs"
+	"google.golang.org/api/option"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"sync"
+	"syscall"
+)
+
+// RunHTTPServer runs an HTTP server listening on the port specified by PORT in the environment.
+// This function will block until the server is running.
+// On SIGINT or SIGTERM the server will be shutdown cleanly.
+func RunHTTPServer(ctx context.Context, createHandler func(router chi.Router) http.Handler) {
+	apiRouter := chi.NewRouter()
+	setMiddlewares(apiRouter)
+
+	rootRouter := chi.NewRouter()
+	rootRouter.Mount("/api", createHandler(apiRouter))
+
+	srv := &http.Server{
+		Addr:    ":" + os.Getenv("PORT"),
+		Handler: rootRouter,
+	}
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		_ = srv.ListenAndServe()
+	}()
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		_ = <-sigs
+		_ = srv.Shutdown(ctx)
+	}()
+
+	wg.Wait()
+}
+
+func setMiddlewares(router *chi.Mux) {
+	router.Use(logs.NewStructuredLogger(logrus.StandardLogger()))
+	addAuthMiddleware(router)
+}
+
+func addAuthMiddleware(router *chi.Mux) {
+	if mockAuth, _ := strconv.ParseBool(os.Getenv("MOCK_AUTH")); mockAuth {
+		logrus.Info("Using JWT mock auth")
+		router.Use(auth.HttpMockMiddleware)
+		return
+	}
+
+	var opts []option.ClientOption
+	if file := os.Getenv("SERVICE_ACCOUNT_FILE"); file != "" {
+		opts = append(opts, option.WithCredentialsFile(file))
+	}
+
+	config := &firebase.Config{ProjectID: os.Getenv("GCP_PROJECT")}
+	firebaseApp, err := firebase.NewApp(context.Background(), config, opts...)
+	if err != nil {
+		logrus.Fatalf("error initializing app: %v\n", err)
+	}
+
+	authClient, err := firebaseApp.Auth(context.Background())
+	if err != nil {
+		logrus.WithError(err).Fatal("Unable to create firebase Auth client")
+	}
+
+	router.Use(auth.FirebaseHttpMiddleware{authClient}.Middleware)
+}
---
+++ internal/common/server/httperr/http_error.go
@@ -0,0 +1,64 @@
+// Package httperr implements a library for dealing with HTTP errors.
+package httperr
+
+import (
+	"github.com/go-chi/render"
+	"github.com/steven-eckhoff/gopher-cache-open/internal/common/errors"
+	"github.com/steven-eckhoff/gopher-cache-open/internal/common/logs"
+	"net/http"
+)
+
+// InternalError sends an ErrorResponse to the client with an internal error status.
+func InternalError(slug string, err error, w http.ResponseWriter, r *http.Request) {
+	httpRespondWithError(err, slug, w, r, "Internal server error", http.StatusInternalServerError)
+}
+
+// Unauthorized sends an ErrorResponse to the client with an unauthorized error status.
+func Unauthorised(slug string, err error, w http.ResponseWriter, r *http.Request) {
+	httpRespondWithError(err, slug, w, r, "Unauthorised", http.StatusUnauthorized)
+}
+
+// BadRequest sends an ErrorResponse to the client with a bad request error status.
+func BadRequest(slug string, err error, w http.ResponseWriter, r *http.Request) {
+	httpRespondWithError(err, slug, w, r, "Bad request", http.StatusBadRequest)
+}
+
+// RespondWithSlugError sends an ErrorResponse to the client based on the ErrorType of the given err.
+// If err is a plain error type then the client will receive an ErrorResponse with an internal error status.
+func RespondWithSlugError(err error, w http.ResponseWriter, r *http.Request) {
+	slugError, ok := err.(errors.SlugError)
+	if !ok {
+		InternalError("internal-server-error", err, w, r)
+		return
+	}
+
+	switch slugError.ErrorType() {
+	case errors.ErrorTypeAuthorization:
+		Unauthorised(slugError.Slug(), slugError, w, r)
+	case errors.ErrorTypeIncorrectInput:
+		BadRequest(slugError.Slug(), slugError, w, r)
+	default:
+		InternalError(slugError.Slug(), slugError, w, r)
+	}
+}
+
+func httpRespondWithError(err error, slug string, w http.ResponseWriter, r *http.Request, logMSg string, status int) {
+	logs.GetLogEntry(r).WithError(err).WithField("error-slug", slug).Warn(logMSg)
+	resp := ErrorResponse{slug, status}
+
+	if err := render.Render(w, r, resp); err != nil {
+		panic(err)
+	}
+}
+
+// ErrorResponse represents the response to the client of the HTTP server.
+type ErrorResponse struct {
+	Slug       string `json:"slug"`
+	httpStatus int
+}
+
+// Render sets the HTTP status in the header.
+func (e ErrorResponse) Render(w http.ResponseWriter, _ *http.Request) error {
+	w.WriteHeader(e.httpStatus)
+	return nil
+}
